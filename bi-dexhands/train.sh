cf6e4ffc7c3892df61a1453b77390483e7efb046
quantumiracle <zhding@mail.ustc.edu.cn>
2022-10-19 22:30:42 -0400

    test

--- bi-dexhands/train.sh
+++ bi-dexhands/train.sh
@@ -2,19 +2,19 @@ DATE=`date '+%Y%m%d_%H%M'`
 echo "Save as: " $DATE
 
 # declare -a tasks=('ShadowHandCatchAbreast' 'ShadowHandOver')
-# declare -a tasks=('ShadowHandPen' 'ShadowHandSwingCup')
+declare -a tasks=('ShadowHandPen' 'ShadowHandSwingCup')
 
-declare -a tasks=('ShadowHandCatchAbreast' 'ShadowHandOver' 'ShadowHandBlockStack' 'ShadowHandCatchUnderarm'
-'ShadowHandCatchOver2Underarm' 'ShadowHandBottleCap' 'ShadowHandLiftUnderarm' 'ShadowHandTwoCatchUnderarm'
-'ShadowHandDoorOpenInward' 'ShadowHandDoorOpenOutward' 'ShadowHandDoorCloseInward' 'ShadowHandDoorCloseOutward'
-'ShadowHandPushBlock' 'ShadowHandKettle' 'ShadowHandLiftUnderarm' 'ShadowHandPointCloud' 'ShadowHandReOrientation'
-'ShadowHandScissors' 'ShadowHandPen' 'ShadowHandSwingCup' 'ShadowHandBottleCap' 'ShadowHandGraspAndPlace' 'ShadowHandSwitch'
-)
+# declare -a tasks=('ShadowHandCatchAbreast' 'ShadowHandOver' 'ShadowHandBlockStack' 'ShadowHandCatchUnderarm'
+# 'ShadowHandCatchOver2Underarm' 'ShadowHandBottleCap' 'ShadowHandLiftUnderarm' 'ShadowHandTwoCatchUnderarm'
+# 'ShadowHandDoorOpenInward' 'ShadowHandDoorOpenOutward' 'ShadowHandDoorCloseInward' 'ShadowHandDoorCloseOutward'
+# 'ShadowHandPushBlock' 'ShadowHandKettle' 'ShadowHandLiftUnderarm' 'ShadowHandPointCloud' 'ShadowHandReOrientation'
+# 'ShadowHandScissors' 'ShadowHandPen' 'ShadowHandSwingCup' 'ShadowHandBottleCap' 'ShadowHandGraspAndPlace' 'ShadowHandSwitch'
+# )
 
 
 mkdir -p log/$DATE
 for i in ${!tasks[@]}; do
 	# nohup python train.py --task=${tasks[$i]} --algo=ppo --record_video=True --wandb_activate=True --wandb_entity=quantumiracle >> log/$DATE/${tasks[$i]}.log &
-	echo nohup python train.py --task=${tasks[$i]} --graphics_device_id=$((i % 8)) --algo=ppo --record_video=True --record_video_interval=30 --wandb_activate=True --wandb_entity=quantumiracle : log/$DATE/${tasks[$i]}.log &
-	nohup python train.py --task=${tasks[$i]} --graphics_device_id=$((i % 8)) --algo=ppo --record_video=True --record_video_interval=30 --wandb_activate=True --wandb_entity=quantumiracle >> log/$DATE/${tasks[$i]}.log &
+	echo nohup python train.py --task=${tasks[$i]} --rl_device=cuda:$((i % 8)) --algo=ppo --record_video=True --record_video_interval=30 --wandb_activate=True --wandb_entity=quantumiracle : log/$DATE/${tasks[$i]}.log &
+	nohup python train.py --task=${tasks[$i]} --rl_device=cuda:$((i % 8)) --algo=ppo --record_video=True --record_video_interval=30 --wandb_activate=True --wandb_entity=quantumiracle >> log/$DATE/${tasks[$i]}.log &
 done
