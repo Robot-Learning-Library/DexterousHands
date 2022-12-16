47ccaba6b39b2a037dddd4919585d750f04109b8
zmsn-1998 <jmji@zju.edu.cn>
2022-12-16 17:15:35 +0800

    add two catch underarm config -> 0.5 entropy

--- bi-dexhands/utils/config.py
+++ bi-dexhands/utils/config.py
@@ -68,6 +68,9 @@ def retrieve_cfg(args, use_rlg_config=False):
                     "ShadowHandPushBlock", "ShadowHandSwingCup", "ShadowHandGraspAndPlace", "ShadowHandScissors", "AllegroHandOver", "AllegroHandCatchUnderarm"]:
         return os.path.join(args.logdir, "{}/{}/{}".format(args.task, args.algo, args.algo)), "cfg/{}/config.yaml".format(args.algo), "cfg/{}.yaml".format(args.task)
 
+    elif args.task in ["ShadowHandTwoCatchUnderarm"]:
+        return os.path.join(args.logdir, "{}/{}/{}".format(args.task, args.algo, args.algo)), "cfg/{}/two_catch_config.yaml".format(args.algo), "cfg/{}.yaml".format(args.task)
+
     elif args.task in ["ShadowHandLiftUnderarm"]:
         return os.path.join(args.logdir, "{}/{}/{}".format(args.task, args.algo, args.algo)), "cfg/{}/lift_config.yaml".format(args.algo), "cfg/{}.yaml".format(args.task)
 
