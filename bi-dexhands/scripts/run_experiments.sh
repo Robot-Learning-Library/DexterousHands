2c22292c831b051c34107716a475c4cbea821890
AlvinYH <yehang@pku.edu.cn>
2023-01-14 03:51:42 +0800

    change my bash

--- bi-dexhands/scripts/run_experiments.sh
+++ bi-dexhands/scripts/run_experiments.sh
@@ -4,22 +4,64 @@ echo "Save as: " $DATE
 # declare -a tasks=('ShadowHand')
 
 declare -a tasks=( 'ShadowHand' 'ShadowHandCatchAbreast' 'ShadowHandOver' 'ShadowHandBlockStack' 'ShadowHandCatchUnderarm'
-'ShadowHandCatchOver2Underarm' 'ShadowHandBottleCap' 'ShadowHandLiftUnderarm' 'ShadowHandTwoCatchUnderarm'
-'ShadowHandDoorOpenInward' 'ShadowHandDoorOpenOutward' 'ShadowHandDoorCloseInward' 'ShadowHandDoorCloseOutward'
-'ShadowHandPushBlock' 'ShadowHandKettle' 
-'ShadowHandScissors' 'ShadowHandPen' 'ShadowHandSwingCup' 'ShadowHandGraspAndPlace' 'ShadowHandSwitch'
+'ShadowHandCatchOver2Underarm' 'ShadowHandBottleCap' 'ShadowHandTwoCatchUnderarm'
+'ShadowHandDoorOpenInward'
 )
 
-# declare -a tasks=('ShadowHandGraspAndPlace' 'ShadowHandKettle' 'ShadowHandBlockStack' 'ShadowHandLiftUnderarm'
-# 'ShadowHandPushBlock' 'ShadowHandSwingCup' 'ShadowHandDoorOpenInward'
-# )
+mkdir -p log/$DATE
 
-# declare -a tasks=('ShadowHandPointCloud'  'ShadowHandReOrientation')  # unrecognized tasks
-# declare -a tasks=('ShadowHandGraspAndPlace' 'ShadowHandSwitch' 'ShadowHandDoorCloseOutward') # not solved
+for i in ${!tasks[@]}; do
+	nohup python train.py --task=${tasks[$i]} --seed=25 --rl_device=cuda:$((i % 5 + 3)) --sim_device=cuda:$((i % 5 + 3)) --graphics_device_id=$((i % 5 + 3)) --algo=ppo --headless --num_envs=2048 --max_iterations=5000 --model_dir=./logs/${tasks[$i]}/ppo/ppo_seed${15}/model_${20000}.pt  >> log/$DATE/${tasks[$i]}.log &
+done
+wait
+
+declare -a tasks=( 'ShadowHandPen' 'ShadowHandGraspAndPlace' 'ShadowHandSwitch' 'ShadowHandDoorOpenOutward' 'ShadowHandDoorCloseInward'
+'ShadowHandPushBlock' 'ShadowHandTwoCatchUnderarm'
+'ShadowHandScissors')
 
 mkdir -p log/$DATE
 for i in ${!tasks[@]}; do
-	# nohup python train.py --task=${tasks[$i]} --algo=ppo --record_video=True --wandb_activate=True --wandb_entity=quantumiracle >> log/$DATE/${tasks[$i]}.log &
-	echo nohup python train.py --task=${tasks[$i]}  --seed=5 --rl_device=cuda:$((i % 8)) --sim_device=cuda:$((i % 8)) --graphics_device_id=$((i % 8)) --algo=ppo --headless --num_envs=2048 --max_iterations=20000 : log/$DATE/${tasks[$i]}.log &
-	nohup python train.py --task=${tasks[$i]} --seed=5 --rl_device=cuda:$((i % 8)) --sim_device=cuda:$((i % 8)) --graphics_device_id=$((i % 8)) --algo=ppo --headless --num_envs=2048 --max_iterations=20000  >> log/$DATE/${tasks[$i]}.log &
+	nohup python train.py --task=${tasks[$i]} --seed=25 --rl_device=cuda:$((i % 5 + 3)) --sim_device=cuda:$((i % 5 + 3)) --graphics_device_id=$((i % 5 + 3)) --algo=ppo --headless --num_envs=2048 --max_iterations=5000 --model_dir=./logs/${tasks[$i]}/ppo/ppo_seed${15}/model_${20000}.pt  >> log/$DATE/${tasks[$i]}.log &
+done
+wait
+
+declare -a tasks=( 'ShadowHand' 'ShadowHandCatchAbreast' 'ShadowHandOver' 'ShadowHandBlockStack' 'ShadowHandCatchUnderarm'
+'ShadowHandCatchOver2Underarm' 'ShadowHandBottleCap' 'ShadowHandTwoCatchUnderarm'
+'ShadowHandDoorOpenInward'
+)
+
+mkdir -p log/$DATE
+for i in ${!tasks[@]}; do
+	nohup python train.py --task=${tasks[$i]} --seed=25 --rl_device=cuda:$((i % 5 + 3)) --sim_device=cuda:$((i % 5 + 3)) --graphics_device_id=$((i % 5 + 3)) --algo=ppo --headless --num_envs=2048 --max_iterations=5000 --model_dir=./logs/${tasks[$i]}/ppo/ppo_seed${15}/model_${20000}.pt  >> log/$DATE/${tasks[$i]}.log &
+done
+wait
+
+declare -a tasks=( 'ShadowHandPen' 'ShadowHandGraspAndPlace' 'ShadowHandSwitch' 'ShadowHandDoorOpenOutward' 'ShadowHandDoorCloseInward'
+'ShadowHandPushBlock' 'ShadowHandTwoCatchUnderarm'
+'ShadowHandScissors')
+
+mkdir -p log/$DATE
+for i in ${!tasks[@]}; do
+	nohup python train.py --task=${tasks[$i]} --seed=25 --rl_device=cuda:$((i % 5 + 3)) --sim_device=cuda:$((i % 5 + 3)) --graphics_device_id=$((i % 5 + 3)) --algo=ppo --headless --num_envs=2048 --max_iterations=5000 --model_dir=./logs/${tasks[$i]}/ppo/ppo_seed${15}/model_${20000}.pt  >> log/$DATE/${tasks[$i]}.log &
+done
+wait
+
+declare -a tasks=( 'ShadowHand' 'ShadowHandCatchAbreast' 'ShadowHandOver' 'ShadowHandBlockStack' 'ShadowHandCatchUnderarm'
+'ShadowHandCatchOver2Underarm' 'ShadowHandBottleCap' 'ShadowHandTwoCatchUnderarm'
+'ShadowHandDoorOpenInward'
+)
+
+mkdir -p log/$DATE
+for i in ${!tasks[@]}; do
+	nohup python train.py --task=${tasks[$i]} --seed=25 --rl_device=cuda:$((i % 5 + 3)) --sim_device=cuda:$((i % 5 + 3)) --graphics_device_id=$((i % 5 + 3)) --algo=ppo --headless --num_envs=2048 --max_iterations=5000 --model_dir=./logs/${tasks[$i]}/ppo/ppo_seed${15}/model_${20000}.pt  >> log/$DATE/${tasks[$i]}.log &
+done
+wait
+
+declare -a tasks=( 'ShadowHandPen' 'ShadowHandGraspAndPlace' 'ShadowHandSwitch' 'ShadowHandDoorOpenOutward' 'ShadowHandDoorCloseInward'
+'ShadowHandPushBlock' 'ShadowHandTwoCatchUnderarm'
+'ShadowHandScissors')
+
+mkdir -p log/$DATE
+for i in ${!tasks[@]}; do
+	nohup python train.py --task=${tasks[$i]} --seed=25 --rl_device=cuda:$((i % 5 + 3)) --sim_device=cuda:$((i % 5 + 3)) --graphics_device_id=$((i % 5 + 3)) --algo=ppo --headless --num_envs=2048 --max_iterations=5000 --model_dir=./logs/${tasks[$i]}/ppo/ppo_seed${15}/model_${20000}.pt  >> log/$DATE/${tasks[$i]}.log &
 done
